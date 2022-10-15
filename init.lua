e779bf525f6d89fdafc998833d4d0b67e10251a8
limshenjie <limshenjie@hotmail.com>
2022-10-15 09:50:28 +0800

    Added lua language server

--- init.lua
+++ init.lua
@@ -16,11 +16,50 @@ Plug 'williamboman/mason-lspconfig.nvim'
 Plug 'neovim/nvim-lspconfig'
 vim.call('plug#end')
 
+vim.cmd([[
+autocmd VimEnter *
+  \  if len(filter(values(g:plugs), '!isdirectory(v:val.dir)'))
+  \|   PlugInstall --sync | q
+  \| endif
+  ]])
+
 --startup time
 require('impatient')
+
 --Starting Language servers
 require("mason").setup()
-require("mason-lspconfig").setup()
+require("mason-lspconfig").setup({
+    ensure_installed = {"sumneko_lua"}
+})
+
 --colorscheme 
 vim.cmd([[colorscheme gruvbox]])
 require('custom-gruvbox')
+
+--config lua language server
+require'lspconfig'.sumneko_lua.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+
+-- start lua language server
+require'lspconfig'.sumneko_lua.setup{}
+
