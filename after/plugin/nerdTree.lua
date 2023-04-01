b16df9bb27f4965a34cd20df1df21c2ee1743d49
limshenjie <limshenjie@hotmail.com>
2023-04-01 21:30:27 +0800

    Nerdtree turn off auto on

--- after/plugin/nerdTree.lua
+++ after/plugin/nerdTree.lua
@@ -3,7 +3,6 @@ nnoremap <leader>n :NERDTreeFocus<CR>
 nnoremap <C-n> :NERDTree<CR>
 nnoremap <C-t> :NERDTreeToggle<CR>
 nnoremap <C-f> :NERDTreeFind<CR>
-autocmd VimEnter * NERDTree
 
 autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
 
