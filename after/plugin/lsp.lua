42c1afac5403083ff777e5bfb4f97d1ffc41f5b6
limshenjie <limshenjie@hotmail.com>
2023-04-01 21:25:24 +0800

    Fixed updates to lua lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,25 +1,13 @@
---config lua language server
-require('lspconfig').lua_ls.setup {
-    settings = {
-        capabilities = CAPABILITIES,
-        Lua = {
-            runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT',
-            },
-            diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = {'vim'},
-            },
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
-            },
-            -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = {
-                enable = false,
-            },
-        },
-    },
-}
+--Starting Language servers
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+-- (Optional) Configure lua language server for neovim
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+lsp.setup()
+
 
--- init.lua
+++ init.lua
@@ -1,5 +1,14 @@
+-- Package plugins
+
 local Plug = vim.fn['plug#']
-vim.call('plug#begin','~/AppData/Local/nvim/plugged')
+-- change directory depending on OS
+local pluginPath
+if vim.fn.has("win32") == 1 then
+  pluginPath = vim.loop.os_homedir() .. '\\AppData\\Local\\nvim\\plugged'
+else
+  pluginPath = vim.loop.os_homedir() .. '/.config/nvim/plugged'
+end
+vim.call('plug#begin', pluginPath)
 Plug 'dracula/vim'
 Plug 'preservim/nerdcommenter'
 Plug 'nvim-lua/plenary.nvim'
@@ -12,7 +21,7 @@ Plug 'ryanoasis/vim-devicons'
 Plug 'ellisonleao/gruvbox.nvim'
 Plug 'lewis6991/impatient.nvim'
 Plug 'williamboman/mason.nvim'
-Plug 'williamboman/mason-lspconfig.nvim'
+Plug('williamboman/mason-lspconfig.nvim', {['do'] = ':MasonUpdate'})
 Plug 'neovim/nvim-lspconfig'
 Plug 'hrsh7th/cmp-nvim-lsp'
 Plug 'hrsh7th/cmp-buffer'
@@ -22,6 +31,8 @@ Plug 'hrsh7th/nvim-cmp'
 Plug 'hrsh7th/cmp-vsnip'
 Plug 'hrsh7th/vim-vsnip'
 Plug 'mfussenegger/nvim-jdtls'
+Plug 'L3MON4D3/LuaSnip' 
+Plug('VonHeikemen/lsp-zero.nvim', {['branch'] = 'v2.x'})
 vim.call('plug#end')
 
 --auto download plugins if missing
@@ -65,81 +76,4 @@ require("gruvbox").setup({
 })
 vim.cmd("colorscheme gruvbox")
 require('custom-gruvbox')
-
--- Set up nvim-cmp.
-local cmp = require'cmp'
-
-cmp.setup({
-    snippet = {
-        -- REQUIRED - you must specify a snippet engine
-        expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-        end,
-    },
-    window = {
-        -- Cmp = cmp.config.window.bordered(),
-        -- documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
-    sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'vsnip' }, -- For vsnip users.
-        -- { name = 'luasnip' }, -- For luasnip users.
-        -- { name = 'ultisnips' }, -- For ultisnips users.
-        -- { name = 'snippy' }, -- For snippy users.
-    }, {
-        { name = 'buffer' },
-    })
-})
-
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-        { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-    }, {
-        { name = 'buffer' },
-    })
-})
-
--- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-        { name = 'buffer' }
-    }
-})
-
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-        { name = 'path' }
-    }, {
-        { name = 'cmdline' }
-    })
-})
-
-
--- Set up lspconfig.
-CAPABILITIES = require('cmp_nvim_lsp').default_capabilities()
-
---Starting Language servers
-require("mason").setup({
-    log_level = vim.log.levels.DEBUG
-})
-require("mason-lspconfig").setup({
-    ensure_installed = {"lua_ls"}
-})
-
-
-
 
