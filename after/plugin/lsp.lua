d002c5078d5f9425d9b12f576f5daecfd276358d
limshenjie <limshenjie@hotmail.com>
2023-05-25 00:54:17 +0800

    Added ensure install for lsp-zero

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -8,6 +8,7 @@ end)
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
+    lsp.ensure_installed = {"lua_ls", "denols","tsserver"}
 lsp.setup()
 
 
